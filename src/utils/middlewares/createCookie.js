32d3a51ec315e3397d3ef822e0f84027bb7bd786
JuanjoCieri <juin@hotmail.com.ar>
2023-06-15 20:11:42 -0300

    Fix cookie

--- src/utils/middlewares/createCookie.js
+++ src/utils/middlewares/createCookie.js
@@ -1,6 +1,6 @@
 const createCookie = async (req, res, next) => {
   const cookie = req.cookies.res_sess;
-  if (!cookie) res.cookie("res_sess", "0", { sameSite: 'none', secure: true});
+  if (!cookie) res.cookie("res_sess", "0");
   next();
 };
 
