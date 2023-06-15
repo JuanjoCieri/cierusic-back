b22edcd830ed6f4b92f1d3e4cff0e39b0fcd6f4b
JuanjoCieri <juin@hotmail.com.ar>
2023-06-15 18:41:46 -0300

    Allow cors domain

--- src/utils/middlewares/setHeaders.js
+++ src/utils/middlewares/setHeaders.js
@@ -1,5 +1,5 @@
 function setHeaders(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Origin", "https://cierusic.vercel.app/"); 
     res.header("Access-Control-Allow-Credentials", "true");
     res.header(
       "Access-Control-Allow-Headers",
