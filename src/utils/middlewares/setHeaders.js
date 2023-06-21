99c0fedb528e2d634a1e54f5bc81c16a45641af9
JuanjoCieri <juin@hotmail.com.ar>
2023-06-21 09:44:24 -0300

    s

--- src/utils/middlewares/setHeaders.js
+++ src/utils/middlewares/setHeaders.js
@@ -1,5 +1,5 @@
 function setHeaders(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://cierusic.vercel.app"); 
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173/"); 
     res.header("Access-Control-Allow-Credentials", "true");
     res.header(
       "Access-Control-Allow-Headers",
