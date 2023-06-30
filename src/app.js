2831514a96a00ece30ae7165abdc3c98b3fc426a
JuanjoCieri <juin@hotmail.com.ar>
2023-06-30 09:37:34 -0300

    working

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(setHeaders);
 app.use(
   cors({
     credentials: true,
-    origin: "https://cierusic.vercel.app/",
+    origin: "http://localhost:5173",
     withCredentials: true
   })
 );
